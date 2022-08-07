055fa0d4f395dbf0a9d386747ec0f0561373af1c
walking-sunset <>
2022-08-08 01:12:32 +0900

    rebase: master branch의 commit 합침

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { HealthModule } from './health/health.module';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: isProduction ? 'mysql' : 'sqlite',
+      host: process.env.RDS_HOST,
+      port: 3306,
+      username: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      database: isProduction ? process.env.RDS_DATABASE : 'db.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TodosModule,
+    AuthModule,
+    UsersModule,
+    HealthModule,
+  ],
 })
 export class AppModule {}
