9d8857f569fdfd6919f45a473f87f822bcfafeef
SIYEA J <siyea0405@gmail.com>
2023-04-09 01:40:48 +0900

    feat: signin router 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import SignupPage from './pages/SignupPage';
+import SigninPage from './pages/SigninPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="signup" element={<SignupPage />} />
+          <Route path="signin" element={<SigninPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
