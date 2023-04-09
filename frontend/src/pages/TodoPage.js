cce2fc20c3790ebea3287087954004ba5a385d18
SIYEA J <siyea0405@gmail.com>
2023-04-09 20:20:22 +0900

    feat: token이 없으면 /signin 으로 리다이랙트 기능 추가

--- frontend/src/pages/TodoPage.js
+++ frontend/src/pages/TodoPage.js
@@ -1,4 +1,14 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const TodoPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (!token) {
+      navigate('/signin');
+    }
+  }, []);
   return (
     <>
       <h2>Todo 페이지</h2>
