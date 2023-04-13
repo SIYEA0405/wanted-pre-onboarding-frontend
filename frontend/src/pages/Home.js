890fd6f17e32c4b40cedf0f125012629a4541a83
SIYEA J <siyea0405@gmail.com>
2023-04-13 23:30:36 +0900

    fix: Layout component 적용 및 component 분리 후 코드 리팩토링

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Stack, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import Layout from '../components/Layout';
 
 function Home() {
+  const [token, setToken] = useState(
+    localStorage.getItem('access_token') || '',
+  );
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (token) {
+      navigate('/todo');
+    }
+  }, []);
+
   return (
-    <>
-      <h1>Home 화면 입니다.</h1>
-      <ul>
-        <li>
-          <Link to="/signup">회원가입</Link>
-        </li>
-        <li>
+    <Layout>
+      <Stack spacing={6} direction={'row'}>
+        <Button
+          rounded={'full'}
+          px={6}
+          bg={'#A9B8AF'}
+          _hover={{ bg: '#F1B6B6' }}
+        >
           <Link to="/signin">로그인</Link>
-        </li>
-        <li>
-          <Link to="/todo">Todo</Link>
-        </li>
-      </ul>
-    </>
+        </Button>
+        <Button
+          rounded={'full'}
+          px={6}
+          bg={'#C7D3C7'}
+          _hover={{ bg: '#FFDEDB' }}
+        >
+          <Link to="/signup">회원가입</Link>
+        </Button>
+      </Stack>
+    </Layout>
   );
 }
 
