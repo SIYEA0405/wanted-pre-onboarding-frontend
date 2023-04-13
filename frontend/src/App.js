bfc844fc0f2a9df5bdb8a6feda8133c246153743
SIYEA J <siyea0405@gmail.com>
2023-04-13 23:28:46 +0900

    feat: todo router 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
+import TodoPage from './pages/TodoPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="signup" element={<SignupPage />} />
             <Route path="signin" element={<SigninPage />} />
+            <Route path="/todo" element={<TodoPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
